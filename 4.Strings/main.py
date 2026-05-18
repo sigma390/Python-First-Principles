de5b27cec9c4f8457239cbcb7e56ce37c05e5e8f
sigma390 <sigmacoder390@gmail.com>
2026-05-18 23:51:31 +0530

    Add main.py for string manipulation demonstrations

---
+++ 4.Strings/main.py
@@ -0,0 +1,9 @@
+hello = "HEllow"
+
+print(hello.upper())
+print(hello.lower())
+print(hello.capitalize())
+print(hello.count("ll")) #count no of occurances of given string in desired string
+print(hello * 2)
+print(hello + "world")
+print(type(hello));
