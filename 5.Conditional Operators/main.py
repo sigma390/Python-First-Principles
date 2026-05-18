69742cc5b80c99030d69c20e12e61e30d7ed10fd
sigma390 <sigmacoder390@gmail.com>
2026-05-19 00:10:00 +0530

    Add main.py for conditional operators demonstration

---
+++ 5.Conditional Operators/main.py
@@ -0,0 +1,16 @@
+
+
+# == , != , >= , <= , > , < and , or , not
+
+print(ord('a')); #ascii code 
+
+x = 7
+y = 8
+z = 0
+
+res1 = x == y
+res2 = y > x
+res3 = z < x + 2
+res4 = res1 or res2 and res3
+res5 = not res4
+print(res1 , res2 , res3 , res4 , res5  )
---
+++ 6.Conditionals/main.py
@@ -0,0 +1,18 @@
+
+
+# if else , elif
+
+res = 3 > 5
+
+if res : 
+    print("i told u i would win")
+else   :
+    print("However i wud have won")
+
+
+
+if res == "Tim" :
+    print("You are great")
+elif res== False:
+    print("You are not great")
+
