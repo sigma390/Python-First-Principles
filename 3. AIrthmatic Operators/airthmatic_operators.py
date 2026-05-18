7aba764f2c7a0b904a7f6746edae89b469cb80d6
sigma390 <sigmacoder390@gmail.com>
2026-05-18 22:17:08 +0530

    Add airthmatic_operators.py for basic arithmetic operations and user input handling

---
+++ 3. AIrthmatic Operators/airthmatic_operators.py
@@ -0,0 +1,42 @@
+x = 10
+y = 11
+
+
+
+#addition
+z = x + y
+
+#multiplication
+
+p = x * y
+
+#division
+
+q = x / y
+
+# ceiling div
+
+r = x // y
+
+#power
+
+s = x ** y
+
+
+#mod
+
+o = x % y
+
+
+# BODMAS rule always
+
+k = (x + y) * 4
+
+
+j = int(input("Enter the number :"))
+l = j *7;
+print("this is value Of L :", l);
+
+
+print( z, "\n" ,p , "\n" ,q , "\n" ,r , "\n" ,s, "\n", o )
+print(k)
