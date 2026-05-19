16aa1f84efdc828134a4fc44c271a8241c481674
sigma390 <sigmacoder390@gmail.com>
2026-05-19 23:05:39 +0530

    Enhance string operations demonstration in main.py and add slicing examples

--- 4.Strings/main.py
+++ 4.Strings/main.py
@@ -4,6 +4,8 @@ print(hello.upper())
 print(hello.lower())
 print(hello.capitalize())
 print(hello.count("ll")) #count no of occurances of given string in desired string
+print(hello.find("E"))
 print(hello * 2)
 print(hello + "world")
+print(hello.casefold()) # aggressively lowers the case 
 print(type(hello));
--- 9.Slice Operator/main.py
+++ 9.Slice Operator/main.py
@@ -7,6 +7,13 @@ x  = [0,1,8,3,4,5,6,7,8,9]
 sl_1 = x[:5]; #default step is 1
 sl_2 = x[1:7:2] #stepping forwards
 sl_3 = x[7:0:-1] #stepping backwards
+
+#simplest method to reverse a list or a string
+
+y = x[::-1];
+
+
+
 for x in sl_1:
     print(x)
 
