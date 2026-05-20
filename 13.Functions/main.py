21c8f2a89c8f0e9c094cb82bc03829e65f0cab4e
sigma390 <sigmacoder390@gmail.com>
2026-05-20 23:10:55 +0530

    Add main.py for demonstrating function definitions and return values in Python

---
+++ 13.Functions/main.py
@@ -0,0 +1,37 @@
+
+
+#functions accept args
+#functions are Objects means we can return a function
+
+#normal function
+def func():
+    print("We are inside a function")
+
+func()
+
+
+#function inside a function and calls  , also Optional params
+def sum (x , y , z , q = None):
+    s = x + y
+    def multi(s , z):
+        return s*z;
+    return multi(s,z)
+
+
+
+#function can return multiple values
+def tup(x , y):
+    return x*y, x+y , x**y
+
+
+r1,r2,r3 = tup(2,6)
+
+
+print(tup(2,6))
+print(sum(2,3, 9))
+print(r1,r2,r3)
+
+
+
+
+
