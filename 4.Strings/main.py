961d414ce137d244b1d3235965e50e901fddf26d
sigma390 <sigmacoder390@gmail.com>
2026-06-18 10:19:39 +0530

    Add comment to clarify the behavior of the find method in main.py

--- 4.Strings/main.py
+++ 4.Strings/main.py
@@ -4,7 +4,7 @@ print(hello.upper())
 print(hello.lower())
 print(hello.capitalize())
 print(hello.count("ll")) #count no of occurances of given string in desired string
-print(hello.find("E"))
+print(hello.find("E")) #return 1 or 0 based upon existance
 print(hello * 2)
 print(hello + "world")
 print(hello.casefold()) # aggressively lowers the case 
